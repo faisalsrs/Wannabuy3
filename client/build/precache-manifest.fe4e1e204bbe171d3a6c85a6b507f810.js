66f7a1618464f49883648fb45a447c93984e9372
faisalsrs <faisalsrs@gmail.com>
2020-03-27 13:26:19 -0700

    2nd update with Build

---
+++ client/build/precache-manifest.fe4e1e204bbe171d3a6c85a6b507f810.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68fe6979b784430b24a9184a3c3f5efd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2bdd6ce99aa28f44778",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e79a6fb6d80ca805be5e",
+    "url": "/static/css/main.2527a9ef.chunk.css"
+  },
+  {
+    "revision": "b2bdd6ce99aa28f44778",
+    "url": "/static/js/2.9ab5e3d8.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.9ab5e3d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e79a6fb6d80ca805be5e",
+    "url": "/static/js/main.f44a9690.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
--- client/src/views/Items.js
+++ client/src/views/Items.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+import moment from "moment";
 import { Link } from "@reach/router";
 
 const Items = props => {
@@ -48,51 +49,55 @@ const Items = props => {
 
   return (
     <>
-      <div>
-        <h3>What are you looking to buy?</h3>
-        <hr />
-        <table>
-          <thead>
-            <tr>
-              <th>Item Name</th>
-              <th>Description</th>
-              <th>Condition</th>
-              <th>Item Image</th>
-              <th>Date</th>
-              <th>Current Bid Price</th>
-              <th>Bid</th>
-            </tr>
-          </thead>
-          <tbody>
-            {items.map((item, idx) => (
-              <tr key={idx}>
-                <td>{item.name}</td>
-                <td>{item.desc}</td>
-                <td>{item.condition}</td>
-                <td>
-                  <img
-                    style={{
-                      paddingBottom: 20,
-                      borderBottom: "2px solid gray"
-                    }}
-                    width="20%"
-                    src={item.imageURL}
-                    alt={`${item.name} item`}
-                  />
-                </td>
-                <td>{item.date}</td>
-                <td>{item.price}</td>
-                <td>
-                  <Link to={"/wannabuys/" + item._id}>Want to bid?</Link>{" "}
-                  {/* <Link to={"/pets/" + pet._id + "/edit"}>Edit</Link> |{" "} */}
-                  {/* <button onClick={event => handleDelete(pet._id)}>
+      <div class="container">
+        <div class="row">
+          <h3>What are you looking to buy?</h3>
+          <hr />
+          <div style={{ textAlign: "center" }}>
+            <table>
+              <thead>
+                <tr>
+                  <th scope="col">Item Name</th>
+                  <th scope="col">Description</th>
+                  <th scope="col">Condition</th>
+                  <th scope="col">Item Image</th>
+                  <th scope="col-sm">Date</th>
+                  <th scope="col">Current Bid Price</th>
+                  <th scope="col">Bid</th>
+                </tr>
+              </thead>
+              <tbody>
+                {items.map((item, idx) => (
+                  <tr key={idx}>
+                    <td>{item.name}</td>
+                    <td>{item.desc}</td>
+                    <td>{item.condition}</td>
+                    <td>
+                      <img
+                        style={{
+                          paddingBottom: 20,
+                          borderBottom: "2px solid gray"
+                        }}
+                        width="150px"
+                        src={item.imageURL}
+                        alt={`${item.name} item`}
+                      />
+                    </td>
+                    <td>{moment(item.date).format("MMMM D, YYYY")}</td>
+                    <td>{item.price}</td>
+                    <td>
+                      <Link to={"/wannabuys/" + item._id}>Want to bid?</Link>{" "}
+                      {/* <Link to={"/pets/" + pet._id + "/edit"}>Edit</Link> |{" "} */}
+                      {/* <button onClick={event => handleDelete(pet._id)}>
                     Adopt
                   </button> */}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </>
   );
